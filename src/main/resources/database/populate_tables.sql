c9afd457709182d0c236ae00a2aa1ea7a414fa3e
Vincent Bonamy <vbonamy@gmail.com>
2014-06-11 17:09:03 +0200

    Fix: inversion ROL_ID , FCT_ID in populate_tables.sql

--- src/main/resources/database/populate_tables.sql
+++ src/main/resources/database/populate_tables.sql
@@ -14,13 +14,13 @@ INSERT INTO role VALUES (2, 'ROLE_REPORT');
 INSERT INTO role VALUES (3, 'ROLE_SUPER_ADMIN');
 
 -- Association des fonctions aux rôles
-INSERT INTO role_composition VALUES (1, 2);
-INSERT INTO role_composition VALUES (1, 3);
-INSERT INTO role_composition VALUES (2, 1);
-INSERT INTO role_composition VALUES (3, 1);
-INSERT INTO role_composition VALUES (3, 2);
-INSERT INTO role_composition VALUES (3, 3);
-INSERT INTO role_composition VALUES (3, 4);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (1, 2);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (1, 3);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (2, 1);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (3, 1);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (3, 2);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (3, 3);
+INSERT INTO role_composition (ROL_ID, FCT_ID) VALUES (3, 4);
 
 -- Ajout du premier super administrateur
 INSERT INTO user_bo_smsu VALUES (1, 'admin', 3);

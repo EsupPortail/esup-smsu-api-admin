e8dc81b13e87720d61cfbd23e2b894806691388c
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2014-06-12 17:16:30 +0200

    create_tables.sql: update using "ant generate-ddl"

--- src/main/resources/database/create_tables.sql
+++ src/main/resources/database/create_tables.sql
@@ -1,6 +1,6 @@
 create table fonction (FCT_ID integer not null auto_increment, FTC_NAME varchar(32) not null unique, primary key (FCT_ID)) ENGINE=InnoDB;
 create table role (ROL_ID integer not null auto_increment, ROL_NAME varchar(32) not null unique, primary key (ROL_ID)) ENGINE=InnoDB;
-create table role_composition (FCT_ID integer not null, ROL_ID integer not null, primary key (ROL_ID, FCT_ID)) ENGINE=InnoDB;
+create table role_composition (ROL_ID integer not null, FCT_ID integer not null, primary key (ROL_ID, FCT_ID)) ENGINE=InnoDB;
 create table user_bo_smsu (USER_ID integer not null auto_increment, USER_LOGIN varchar(32) not null unique, ROL_ID integer not null, primary key (USER_ID)) ENGINE=InnoDB;
 alter table role_composition add index FK7545E7617AA0C267 (ROL_ID), add constraint FK7545E7617AA0C267 foreign key (ROL_ID) references role (ROL_ID);
 alter table role_composition add index FK7545E761774B10BB (FCT_ID), add constraint FK7545E761774B10BB foreign key (FCT_ID) references fonction (FCT_ID);

3d53804ca5ab73e19e9772c64d6124984a9b07cb
Boris <boris.goming@univ-paris1.fr>
2018-11-20 17:21:59 +0100

    WIP Helper.java is modified with ResponseEntity, HttpStatus and MultiValueMap of org.springframework.util.

--- src/main/java/org/esupportail/smsuapiadmin/web/controllers/exceptionmappers/Helper.java
+++ src/main/java/org/esupportail/smsuapiadmin/web/controllers/exceptionmappers/Helper.java
@@ -1,14 +1,23 @@
 package org.esupportail.smsuapiadmin.web.controllers.exceptionmappers;
 
-import javax.ws.rs.core.Response;
-
 import org.codehaus.jackson.node.JsonNodeFactory;
 import org.codehaus.jackson.node.ObjectNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 public class Helper {
-    public static Response jsonErrorResponse(int status, String error) {
+	
+    public static ResponseEntity<?> jsonErrorResponse(HttpStatus status, String error) {
         ObjectNode node = JsonNodeFactory.instance.objectNode();
         node.put("error", error);
-        return Response.status(status).entity(node).type("application/json").build();
+        return new ResponseEntity<ObjectNode>(node, headers("Content-Type", "application/json;charset=UTF-8"), status);
+    }
+    
+    private static MultiValueMap<String, String> headers(String k, String v){
+    	MultiValueMap<String, String> r = new LinkedMultiValueMap<>();
+    	r.add(k, v);
+    	return r;
     }
 }

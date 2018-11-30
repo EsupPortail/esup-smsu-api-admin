efd931cd19742097966281a7d5507b8e35e29d25
Boris <boris.goming@univ-paris1.fr>
2018-11-30 15:31:33 +0100

    WIP Creation of TransactionManagerFilter class offering the Hibernate transaction opening/closing services per request.
    If commit issues of database transactions arise, a rollback is executed.

---
+++ src/main/java/org/esupportail/smsu/web/TransactionManagerFilter.java
@@ -0,0 +1,41 @@
+package org.esupportail.smsu.web;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.esupportail.smsu.utils.HibernateUtils;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class TransactionManagerFilter implements Filter {
+
+	@Inject
+	private SessionFactory sessionFactory;
+
+    public void destroy() {}
+    public void init(FilterConfig config) {}
+
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain filterChain) throws IOException, ServletException {
+        boolean participate = HibernateUtils.openSession(sessionFactory);
+        Transaction transaction = sessionFactory.getCurrentSession().beginTransaction();
+        try {
+    		filterChain.doFilter(request, response);
+        } finally {
+        	try {
+        		transaction.commit();
+        	}
+            catch (Exception e) {
+            	transaction.rollback();
+			}
+            HibernateUtils.closeSession(sessionFactory, participate);
+        }
+    }
+
+}

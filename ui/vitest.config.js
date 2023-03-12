bfd438d4a4f43481f9db45d13cb6b8301b3fce7e
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2023-03-12 16:43:30 +0100

    configure vitest for unit tests

---
+++ ui/vitest.config.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      // @ts-ignore
+      root: fileURLToPath(new URL('./', import.meta.url))
+    }
+  })
+)
